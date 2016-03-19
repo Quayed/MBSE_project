fb260867e0533c7d5383ada9798905ee005d8cc3
Mathias Sondrup <matsondrup@gmail.com>
2016-03-19 16:07:53 +0100

    init

---
+++ src/yawl/Yawl/impl/YAWLNetImpl.java
@@ -0,0 +1,47 @@
+/**
+ */
+package yawl.Yawl.impl;
+
+import org.eclipse.emf.ecore.EClass;
+import org.pnml.tools.epnk.pnmlcoremodel.impl.PetriNetTypeImpl;
+import yawl.Yawl.YAWLNet;
+import yawl.Yawl.YawlPackage;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>YAWL Net</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ * @generated
+ */
+public class YAWLNetImpl extends PetriNetTypeImpl implements YAWLNet {
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated NOT
+	 */
+	public YAWLNetImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected EClass eStaticClass() {
+		return YawlPackage.Literals.YAWL_NET;
+	}
+
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated NOT
+	 */
+	public String toString() {
+		return "dk.mdsu.groupC.yawlNet";
+	}
+
+} //YAWLNetImpl
